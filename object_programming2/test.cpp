905c9369a589a479f4b2074c7c1929f864d2c65e
minju <71gnos@naver.com>
2023-10-19 15:37:50 +0900

    실습

--- object_programming2/test.cpp
+++ object_programming2/test.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-	bool b;
-	b = (1 == 2);
 
-	cout << std::boolalpha;
-	cout << b;
-
+	int i = 0;
+	for (;;) {
+		if (i > 10) break;
+		if (i < 6) continue;
+		printf("%d", i);
+		i++;
+	}
+	
 	return 0;
 }
