447d02a993059eacd5ba699e04cbcbb2238ad136
minju <71gnos@naver.com>
2023-12-26 10:21:40 +0900

    ass

--- object_programming2/test.cpp
+++ object_programming2/test.cpp
@@ -2,28 +2,28 @@
 
 using namespace std;
 
-class C {
-private:
-	double r, i;
-
+class Complex {
 public:
-	C(double r = 0.0, double i = 0.0) :r(r), i(i) { }
-
-	double getReal() { return r; }
-	double getImag() { return i; }
-
-	C operator+=(C& obj);
+	double real, imag;
+	Complex(double r = 0, double i = 0) :real(r), imag(i) {
+		cout << "생성자 호출" << endl;
+	}
+	~Complex() { cout << "소멸자 호출" << endl; }
+	void print() {
+		cout << real << "+" << imag << "i" << endl;
+	}
 };
-C C::operator+=(C& obj) {
-	return (this->r + obj.r, this->i + obj.i);
+Complex add(Complex c1, Complex c2) {
+	return Complex(c1.real + c2.real,c1.imag + c2.imag);
 }
+
 int main() {
 
-	C a(1.1, 1.1), b(1.1, 1.1);
+	Complex c1{ 1,2 }, c2{ 3, 4 };
+	Complex t;
 
-	a += b;
-
-	cout << a.getReal() << " " << a.getImag();
+	t = add(c1, c2);
+	t.print();
 
 	return 0;
 }
