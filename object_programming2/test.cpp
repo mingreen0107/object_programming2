e72eb48ed417ef6ed546d4807330ec3c255bc3df
minju <71gnos@naver.com>
2024-01-25 22:59:49 +0900

    ass

--- object_programming2/test.cpp
+++ object_programming2/test.cpp
@@ -5,9 +5,7 @@ using namespace std;
 class Complex {
 public:
 	double real, imag;
-	Complex(double r = 0, double i = 0) :real(r), imag(i) {
-		cout << "생성자 호출" << endl;
-	}
+	Complex(double r = 0, double i = 0) :real(r), imag(i) { cout << "생성자 호출" << endl;	}
 	~Complex() { cout << "소멸자 호출" << endl; }
 	void print() {
 		cout << real << "+" << imag << "i" << endl;
